9f3f88c05722132ab491c34a1b07deac618d4a6e
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-05-23 23:30:51 -0400

    clarified fetch order, cannot fetch-set-fetch in one codeblock

--- src/components/Datafetcher.jsx
+++ src/components/Datafetcher.jsx
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function Datafetcher() {
+  
+  //Methinks that the actual returned component cant be accessed, set and passed to another object all in the same command frame, needs to be broken up into more components.
 
   const [incidents, setIncidents] = useState([]);
   const [geoCode, setGeoCode] = useState({});
   
 
-  const reverseGeoCode = async (search) => {
+  const reverseGeoCode = async (lat, lon) => {
     //logic to include search term in api call  
     const params = {
       access_key: 'd082a061bc23cc199563ea78cab61972',
-      query: "1600 Pennsylvania Ave NW, Washington DC"
-        // `${search}`
+      query: `${lat},${lon}`
     }
 
     let response = await axios.get(
-      `http://api.positionstack.com/v1/forward`, { params }
+      `http://api.positionstack.com/v1/reverse`, { params }
     );
-      console.log(response.data);
+      console.log(response.data.data[0].name);
   }  
 
   const fetchIncident = async () => {
@@ -31,7 +32,8 @@ function Datafetcher() {
   useEffect(() => {
     
     fetchIncident();
-    reverseGeoCode();
+    // console.log("lets log the" + incidents[0].lat, incidents[0].lon);
+    reverseGeoCode(26.768562 , -80.0497);
   }, []);
 
     return (
