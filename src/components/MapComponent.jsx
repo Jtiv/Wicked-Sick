7a9397e0af50e02c54d00c8e0fb2082b88c017d9
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-06-10 10:28:08 -0400

    added empty Map component for testing

---
+++ src/components/MapComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from 'react-map-gl';
+
+function MapComponent() {
+  return (
+    <div>
+      <div>MapComponent</div>
+      
+    </div> 
+  )
+}
+
+export default MapComponent
