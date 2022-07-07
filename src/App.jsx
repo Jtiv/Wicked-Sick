e96967cff8cea959c1235dad135e83b7e3b5959c
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-07-07 15:39:44 -0400

    added Mapcomponent, and render it in the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Datafetcher from './components/Datafetcher';
+import MapComponent from './components/MapComponent.jsx';
 
 function App() {
   return (
     <div className="App">
-     <Datafetcher />
-      
-
+    <MapComponent/>
+    {/* <Datafetcher /> */}      
     </div>
   );
 }
--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import Map from 'react-map-gl';
+import React, { useRef, useEffect, useState } from 'react';
+import maplibregl from 'maplibre-gl';
+import './MapComponent.css'; 
 
 function MapComponent() {
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng] = useState(139.753);
+  const [lat] = useState(35.6844);
+  const [zoom] = useState(14);
+  const [API_KEY] = useState("ph3JTDTUCEk1iznShG7x");
+
+  useEffect(() => {
+    if (map.current) return; //stops map from intializing more than once
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: `https://api.maptiler.com/maps/streets/style.json?key=${API_KEY}`,
+      center: [lng, lat],
+      zoom: zoom
+    });
+    
+  });
+
   return (
-    <div>
-      <div>MapComponent</div>
-      
-    </div> 
-  )
+    <div className="map-wrap">
+      <div ref={mapContainer} className="map" />
+    </div>
+  );
+
 }
 
 export default MapComponent
